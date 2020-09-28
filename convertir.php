5bc0ae2a6b6889629d1b8b3b5bed0ee71baa2deb
juanaves <juanaves@gmail.com>
2020-09-28 20:21:03 +0200

    comit 1

---
+++ convertir.php
@@ -0,0 +1,54 @@
+<?php
+if( isset($_POST['convertir']) ) 
+  {
+    $temperatura   = $_POST['temperatura'];
+    $tipo =  $_POST['tipo'];
+    if ($temperatura >=10000)
+    {
+        $error=true;
+    }
+    elseif ($tipo=="celsius" &&
+            $temperatura <= -273.15)
+         {
+           $error=true;
+         }
+         elseif ($tipo=="fahrenheit" &&
+            $temperatura <= -459.67)
+         {
+           $error=true;
+         }
+         else
+              $error=false;
+         
+  if($error==true)
+  {
+      //header("Location: index.php");
+      echo "ERROR a la hora de introducir los datos";
+  }else {
+      //hago todos los calculos
+      if ($tipo=="celsius")
+      {
+          $conversion=32+(1.8*$temperatura);
+          echo $temperatura." grados Celsius son : ".$conversion. " Fahrenheid";
+      }
+      else
+       {
+          $conversion=($temperatura-32)/1.8;
+          echo $temperatura." grados Fahrenheid son : ".$conversion. " Celsius";
+      }
+      
+  
+          
+   
+  }
+  print"<p class='centrado'>
+       <a href='index.php'>
+          <img alt='' src='Imagenes/icon_home.gif' /><br />Regresar
+       </a>
+    </p>";
+  }else {
+      header("Location: index.php");
+  } 
+     
+       
+
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+   print'<!DOCTYPE html>
+<html>
+<head>
+    
+   <title>Convertidor de Temperaturas</title>
+   <meta charset="utf-8" />
+   <link href="default.css" rel="stylesheet" type="text/css" />
+ 
+</head>
+<body>
+<form action="convertir.php" method="post" >
+       <fieldset>
+         <legend>Convertidor de Temperatura Celsius/Fahrenheit</legend>
+         <p>
+            <label for="temperatura">Temperatura</label>
+            <input type="text" name="temperatura" value="" size="5" />
+            
+
+            <select name="tipo">
+                <option value="celsius"</option>Celsius
+                <option value ="fahrenheit"</option>Fahrenheit
+            </select>
+           
+         </p>
+         <p>
+         <input type="submit" value="Convertir" name="convertir" />
+         </p>
+       </fieldset>
+       </form>
+  
+
+  
+     
+</body>
+</html>';     
